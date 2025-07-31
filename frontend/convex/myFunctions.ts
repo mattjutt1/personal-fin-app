be6864cef4ec78add85c2216c28f5818829e059f
mattjutt <mattutt84@gmail.com>
2025-07-30 21:57:55 -0400

    Convex: Add initial Convex functions (sum and hello).

---
+++ frontend/convex/myFunctions.ts
@@ -0,0 +1,18 @@
+import { mutation, query } from "./_generated/server";
+import { v } from "convex/values";
+
+export const sum = query({
+  args: {
+    numbers: v.array(v.float64),
+  },
+  handler: async (ctx, args) => {
+    const totalSum = args.numbers.reduce((acc, num) => acc + num, 0);
+    return { sum: totalSum };
+  },
+});
+
+export const hello = query({
+  handler: async () => {
+    return { message: "Hello from Convex!" };
+  },
+});
